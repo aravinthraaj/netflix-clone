4d5e30b75838bfd716c903dba1405aa347381478
aravinthraaj <aravinthraajk@gmail.com>
2021-09-22 12:23:56 +0200

    security update

--- src/firebaseControl.js
+++ src/firebaseControl.js
@@ -6,6 +6,7 @@ import 'firebase/compat/auth';
 const { 
   REACT_APP_FIREBASE_API_KEY, 
   REACT_APP_FIREBASE_AUTH_DOMAIN, 
+  REACT_APP_FIREBASE_DATABASE_URL,
   REACT_APP_FIREBASE_PROJECT_ID, 
   REACT_APP_FIREBASE_STORAGE_BUCKET, 
   REACT_APP_FIREBASE_MESSAGING_SENDER_ID, 
@@ -15,7 +16,7 @@ const {
 const firebaseConfig = {
   apiKey: REACT_APP_FIREBASE_API_KEY,
   authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: "https://netflix-clone-dd12c-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
   projectId: REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
@@ -23,17 +24,6 @@ const firebaseConfig = {
 };
 
 
-
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBqehK2vWnkS9-v7Rh2bGGpu_Wgkr3YjI8",
-//   authDomain: "netflix-clone-dd12c.firebaseapp.com",
-//   databaseURL: "https://netflix-clone-dd12c-default-rtdb.europe-west1.firebasedatabase.app",
-//   projectId: "netflix-clone-dd12c",
-//   storageBucket: "netflix-clone-dd12c.appspot.com",
-//   messagingSenderId: "864634775014",
-//   appId: "1:864634775014:web:6541040386fb489a3ae227"
-// };
 
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
